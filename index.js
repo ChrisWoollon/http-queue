5a0d71989297ee30d8e7226625d16bb708ae2576
Oddboxx <chris.woollon@hotmail.com>
2017-10-18 22:21:53 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,58 @@
+const https = require('https');
+
+module.exports = class HttpRequester {
+	
+	constructor(wait) {
+		this.wait = wait;
+		this.requests = [];
+	}
+
+	newRequest(url, callback = null, error = null) {
+		delay(this.wait, () => {
+			return this.makeRequest(url,callback,error);
+		});
+	}
+	makeRequest(url,callback = null, error = null) {
+		// console.log("here");
+		https.get(url, (resp) => {
+			let data = '';
+			resp.on('data', (chunk) => {
+				data += chunk;
+			});
+			resp.on('end', () => {
+				if (callback)
+					callback(JSON.parse(data));
+				else 
+					console.log(JSON.parse(data));
+				return data;
+			});
+		}).on("error", (err) => {
+			if (error)
+				error();
+			else
+				console.log("Error: " + err.message);
+			return error;
+		});
+	}
+
+};
+
+var delay = (function() {
+  var queue = [];
+
+  function processQueue() {
+	if (queue.length > 0) {
+	  setTimeout(function (cc) {
+		queue.shift().cb(cc);
+		processQueue();
+	  }, queue[0].delay);
+	}
+  }
+
+  return function delayed(delay, cb) {
+	queue.push({ delay: delay, cb: cb });
+	if (queue.length === 1) {
+	  processQueue();
+	}
+  };
+}());
